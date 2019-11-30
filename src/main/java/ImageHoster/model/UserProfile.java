afa35c44bbd3232061e8802d64fd668dc75a4130
sangiram1 <sangiram1@gmail.com>
2019-11-30 15:14:09 +0530

    Applied Google Style Indentation to the Source Code as per the Evaluation Rubrics.

--- src/main/java/ImageHoster/model/UserProfile.java
+++ src/main/java/ImageHoster/model/UserProfile.java
@@ -10,55 +10,55 @@ import javax.persistence.*;
 @Table(name = "user_profile")
 public class UserProfile {
 
-    //@Id annotation specifies that the corresponding attribute is a primary key
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    //@Column annotation specifies that the attribute will be mapped to the column in the database.
-    //Here the column name is explicitly mentioned as 'id'
-    @Column(name = "id")
-    private Integer id;
+  //@Id annotation specifies that the corresponding attribute is a primary key
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  //@Column annotation specifies that the attribute will be mapped to the column in the database.
+  //Here the column name is explicitly mentioned as 'id'
+  @Column(name = "id")
+  private Integer id;
 
-    @Column(name = "full_name")
-    private String fullName;
+  @Column(name = "full_name")
+  private String fullName;
 
-    @Column(name = "email_address")
-    private String emailAddress;
+  @Column(name = "email_address")
+  private String emailAddress;
 
-    @Column(name = "mobile_number")
-    private String mobileNumber;
+  @Column(name = "mobile_number")
+  private String mobileNumber;
 
-    public UserProfile() {
-    }
+  public UserProfile() {
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    public String getFullName() {
-        return fullName;
-    }
+  public String getFullName() {
+    return fullName;
+  }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
 
-    public String getEmailAddress() {
-        return emailAddress;
-    }
+  public String getEmailAddress() {
+    return emailAddress;
+  }
 
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
 
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
+  public String getMobileNumber() {
+    return mobileNumber;
+  }
 
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
+  public void setMobileNumber(String mobileNumber) {
+    this.mobileNumber = mobileNumber;
+  }
 }
